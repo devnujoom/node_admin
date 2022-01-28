8d40dfef1cf5d79f807800df1e4e004409a57a0b
shabi m f <shabi@adagency.design>
2022-01-29 01:23:49 +0530

    added express and tested

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("testing...")
+import express from 'express';
+import cors from 'cors';
+
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: ["http://localhost:5000"]
+}));
+
+app.get('/',(req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(port,()=>{
+  console.log(`listening on port ${port}`);
+});
